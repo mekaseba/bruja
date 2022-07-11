3c1b3a6bceaa9aef6d2308f9c8fc12ca11ae44d0
Meka Seba <mekasebameka@gmail.com>
2022-07-11 16:28:15 -0700

    fugly puta

---
+++ index.js
@@ -0,0 +1,7 @@
+let errorParagraph = document.getElementById("error")
+    console.log(errorParagraph)
+
+function no() {
+    console.log("button clicked")
+    errorParagraph.textContent = "Gross, ok. *BLINK*"
+}
